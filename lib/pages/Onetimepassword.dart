62a517de05b6c8e0abc5753fa923385683cf0037
MR.NARTERH <merchantdaniel8@gmail.com>
2023-08-27 17:58:57 +0000

    Added+

--- lib/pages/Onetimepassword.dart
+++ lib/pages/Onetimepassword.dart
@@ -21,12 +21,15 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
   _OtpVerificationScreenState(this.verificationId);
   void verifyCode(BuildContext context) async {
     final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
+    String pin = _pinController1.text +
+        _pinController2.text +
+        _pinController3.text +
+        _pinController4.text;
     try {
       // Create a PhoneAuthCredential with the verification code
       PhoneAuthCredential credential = PhoneAuthProvider.credential(
         verificationId: verificationId,
-        smsCode: codeController.text.trim(),
+        smsCode: pin.trim(),
       );
 
       // Sign in with the credential
@@ -43,6 +46,12 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
     }
   }
 
+  TextEditingController _pinController1 = TextEditingController();
+  TextEditingController _pinController2 = TextEditingController();
+  TextEditingController _pinController3 = TextEditingController();
+  TextEditingController _pinController4 = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,11 +65,46 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: "Enter Verification Code"),
             ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                buildPinTextField(_pinController1),
+                SizedBox(width: 16.0),
+                buildPinTextField(_pinController2),
+                SizedBox(width: 16.0),
+                buildPinTextField(_pinController3),
+                SizedBox(width: 16.0),
+                buildPinTextField(_pinController4),
+              ],
+            ),
             ElevatedButton(
               onPressed: () => verifyCode(context),
               child: Text("Verify Code"),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPinTextField(TextEditingController controller) {
+    return Container(
+      width: 50.0,
+      child: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        maxLength: 1,
+        obscureText: true,
+        decoration: InputDecoration(
+          counter: Offstage(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2.0, color: Colors.blue),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2.0, color: Colors.blue),
+          ),
         ),
       ),
     );

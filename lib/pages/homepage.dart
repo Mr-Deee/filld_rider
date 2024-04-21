1b53c126b3693d111b021402fdd45e23150b85da
mrnarterh <merchantdaniel8@gmail.com>
2024-04-21 21:33:21 +0000

    googlemaps

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -73,7 +73,7 @@ class _homepageState extends State<homepage> {
     locatePosition();
 
     // getPicture();
-    // AssistantMethod.getCurrentOnlineUserInfo(context);
+    AssistantMethod.getCurrentOnlineUserInfo(context);
     _requestLocationPermission();
     getCurrentArtisanInfo();
     requestLocationPermission();

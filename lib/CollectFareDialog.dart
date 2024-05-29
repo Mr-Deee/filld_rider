c8c1c4f84d69b64a6e517052cc763b39ed100ec8
mr-DEEE <merchantdaniel8@gmail.com>
2024-05-29 20:45:26 +0000

    sms

--- lib/CollectFareDialog.dart
+++ lib/CollectFareDialog.dart
@@ -1,10 +1,12 @@
 
 import 'dart:convert';
-
+import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'Models/Assistants/assistantmethods.dart';
+
 
 class CollectFareDialog extends StatefulWidget {
   final String? paymentMethod;
@@ -17,13 +19,25 @@ class CollectFareDialog extends StatefulWidget {
 
 class _CollectFareDialogState extends State<CollectFareDialog> {
   String ?paymentMethod;
+  String? funFact = "Your Fun Fact Will be here shortly..";
 
+  Future<void> fetchRandomFunFact() async {
+    final CollectionReference funFactsCollection =
+    FirebaseFirestore.instance.collection('fun_facts');
+    final QuerySnapshot snapshot = await funFactsCollection.get();
+    final List<DocumentSnapshot> documents = snapshot.docs;
+    final int randomIndex = Random().nextInt(documents.length);
+    final DocumentSnapshot randomDoc = documents[randomIndex];
+    setState(() {
+      funFact = randomDoc['details'];
+    });
+  }
   String? facts ="";
 @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchFacts();
+    fetchRandomFunFact();
   }
   int ?fareAmount;
   _CollectFareDialogState({ this.paymentMethod,  this.fareAmount});
@@ -32,7 +46,7 @@ class _CollectFareDialogState extends State<CollectFareDialog> {
   Widget build(BuildContext context) {
     return Dialog(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(32.0),
+        borderRadius: BorderRadius.circular(12.0),
       ),
       backgroundColor: Colors.transparent,
       child: Container(
@@ -40,95 +54,73 @@ class _CollectFareDialogState extends State<CollectFareDialog> {
         width: double.infinity,
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(30.0),
+          borderRadius: BorderRadius.circular(5.0),
         ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            SizedBox(height: 22.0,),
-
-         Text("Fun Facts",  style: TextStyle(fontSize: 16.0,
-             fontWeight: FontWeight.bold,
-             fontFamily: "Brand Bold"),),
-
-            SizedBox(height: 22.0,),
-
-            Divider(thickness: 4.0,),
-
-            SizedBox(height: 16.0,),
-
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text("$facts"??"Your Fun Fact Will be here shortly..", textAlign: TextAlign.center,),
+            SizedBox(
+              height: 22.0,
+            ),
+            Text(
+              "Fun Fact",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 22.0,
+            ),
+            Divider(
+              height: 2.0,
+              thickness: 2.0,
             ),
 
-            SizedBox(height: 30.0,),
-
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 20 ),
+              child: Text(
+                "$funFact",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
               child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24.0),
-                      side:
-                      const BorderSide(color: Colors.white)),
-                ),
+                onPressed: () async {
 
-                onPressed: () async
-                {
                   Navigator.pop(context);
                   Navigator.pop(context);
-                  AssistantMethod.enableHomeTabLiveLocationUpdates();
                 },
-                //color: Colors.deepPurpleAccent,
+                // color: Colors.blue,
                 child: Padding(
                   padding: EdgeInsets.all(17.0),
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("Done", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black),),
+                      Center(
+                          child: Text(
+                            "Done",
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          )),
+                      // SvgPicture.asset(
+                      //     'assets/images/Vector.svg'),
                     ],
                   ),
                 ),
               ),
             ),
-
-            SizedBox(height: 30.0,),
+            SizedBox(
+              height: 30.0,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Future<void> fetchFacts() async {
-    final url = Uri.parse('https://api.api-ninjas.com/v1/facts?limit=1');
-    final headers = {'X-Api-Key': 'C6I2OTzM2wctRCE6KKi8sQ==SYJVNnpVrCDaY5rE'};
 
-    final response = await http.get(url, headers: headers);
-
-    try {
-      final response = await http.get(url, headers: headers);
-
-      if (response.statusCode == 200) {
-        final jsonData = jsonDecode(response.body);
-        print('jsonData: $jsonData');
-        final data  = jsonData[0]['fact'] ;
-
-        print('Type of data: ${data.runtimeType}');
-        print('Value of data: $data');
-
-        setState(() {
-          facts = data;
-        });
-        // _showFactDialog();
-      } else {
-        throw Exception('Failed to load facts');
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
 }
